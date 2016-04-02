8dc6eba4b171cca417ae5a2ba95fa23529436d8c
yash-buch <yash7.git@gmail.com>
2016-04-02 23:02:58 +0530

    TabView updated to scrollable

--- app/src/main/java/com/binc/expensemanager/ui/DisplayFragmentHost.java
+++ app/src/main/java/com/binc/expensemanager/ui/DisplayFragmentHost.java
@@ -1,20 +1,23 @@
 package com.binc.expensemanager.ui;
 
-import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.binc.expensemanager.R;
-import com.binc.expensemanager.adapter.CategoryPagerAdapter;
+import com.binc.expensemanager.util.Constants;
 
 public class DisplayFragmentHost extends Fragment {
 
     ViewPager viewPager;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
     FragmentManager fm;
 
     @Override
@@ -25,10 +28,47 @@ public class DisplayFragmentHost extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_display_fragment_host, container, false);
-        viewPager = (ViewPager) view.findViewById(R.id.pager);
         fm = getChildFragmentManager();
-        viewPager.setAdapter(new CategoryPagerAdapter(fm, getActivity()));
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+
+        setTabs();
+
+        tabLayout.setOnTabSelectedListener(tabSelectedListener);
 
         return view;
+    }
+
+    TabLayout.OnTabSelectedListener tabSelectedListener = new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            String category = tab.getText().toString();
+            Fragment fragment = new DisplayFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(Constants.CATEGORY, category);
+            fragment.setArguments(bundle);
+
+            fm.beginTransaction()
+                    .replace(R.id.fragment_cat, fragment)
+                    .commit();
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    };
+
+    private void setTabs() {
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
     }
 }
